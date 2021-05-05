ee8174d51c046b5eb79df11bde378838fdb57ae2
Anthony Catullo <acatullo4@gmail.com>
2021-05-05 16:06:52 -0500

    add product crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_191128) do
+ActiveRecord::Schema.define(version: 2021_05_05_203603) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.integer "inventory"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
